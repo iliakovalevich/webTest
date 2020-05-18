19c3f20acf865db84045e76ae127890f593a7e07
Ilya Kavalevich <kava2000.ilia@gmail.com>
2020-05-18 23:56:51 +0300

    add1

---
+++ src/main/java/org/example/entity/User.java
@@ -0,0 +1,44 @@
+package org.example.entity;
+
+public class User {
+  private String name;
+  private String email;
+  private int id;
+  private String age;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public void setAge(String age) {
+    this.age = age;
+  }
+
+  public User(String name, String email, String age) {
+
+  }
+}
---
+++ src/main/java/org/example/service/TestBean.java
@@ -0,0 +1,18 @@
+package org.example.service;
+
+public class TestBean {
+    public TestBean(){}
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
